9bd45600ece980031f84d5eba718867596914f61
Mykola Malkov <mmalkov@sipwise.com>
2018-03-16 13:23:35 +0100

    TT#32751 Build MR specific ISO files
    Add MR variable to build scripts
    Use grub.cfg and isolinux.cfg files as templates
    
    Change-Id: I412cf5eabf291165d53f02bc49e7302353fb948b

--- build_templates.sh
+++ build_templates.sh
@@ -4,43 +4,17 @@
 BASE="$(dirname "$0")"
 TEMPLATES="${TEMPLATES:-templates}"
 BOOT="${BASE}/${TEMPLATES}/boot"
-RELEASES=($(grep -v '#' "${BASE}"/releases | cut -d, -f1 ))
-DISTS=($(grep -v '#' "${BASE}"/releases |cut -d, -f2 ))
-CARRIER=($(grep -v '#' "${BASE}"/releases |cut -d, -f3 ))
-MR="${MR:-no}"
+MR="${MR:-trunk}"
+DIST="${DIST:-stretch}"
 
-rm -f "${BOOT}/grub/sipwise_*.cfg"
-rm -f "${BOOT}/isolinux/sipwise_*.cfg"
+echo "*** grub templates for RELEASE:${MR} DIST:${DIST} ***"
 
-for index in ${!RELEASES[*]}; do
-	if [[ ${RELEASES[$index]} =~ ^mr[0-9]\.[0-9]$ ]] && [ "${MR}" = "no" ]; then
-		echo "*** [SKIP] grub template for RELEASE:${RELEASES[$index]} DIST:${DISTS[$index]} ***"
-		echo "*** [SKIP] isolinux template for RELEASE:${RELEASES[$index]} DIST:${DISTS[$index]} ***"
-	else
-		echo "*** grub template for RELEASE:${RELEASES[$index]} DIST:${DISTS[$index]} CARRIER:${CARRIER[$index]} ***"
-		sed -e "s_##VERSION##_${RELEASES[$index]}_g" \
-			-e "s_##DIST##_${DISTS[$index]}_g" \
-			"${BASE}/grub.cfg_ce" >> "${BOOT}/grub/sipwise_ce.cfg"
-		sed -e "s_##VERSION##_${RELEASES[$index]}_g" \
-			-e "s_##DIST##_${DISTS[$index]}_g" \
-			"${BASE}/grub.cfg_pro" >> "${BOOT}/grub/sipwise_pro.cfg"
-		if [ "${CARRIER[$index]}" == "yes" ]; then
-			sed -e "s_##VERSION##_${RELEASES[$index]}_g" \
-				-e "s_##DIST##_${DISTS[$index]}_g" \
-				"${BASE}/grub.cfg_carrier" >> "${BOOT}/grub/sipwise_carrier.cfg"
-		fi
+sed -e "s_##VERSION##_${MR}_g" \
+	-e "s_##DIST##_${DIST}_g" \
+	"${BASE}/grub.cfg" > "${BOOT}/grub/grub.cfg"
 
-		echo "*** isolinux template for RELEASE:${RELEASES[$index]} DIST:${DISTS[$index]} CARRIER:${CARRIER[$index]} ***"
-		sed -e "s_##VERSION##_${RELEASES[$index]}_g" \
-			-e "s_##DIST##_${DISTS[$index]}_g" "${BASE}/isolinux.cfg_ce" \
-			 >> "${BOOT}/isolinux/sipwise_ce.cfg"
-		sed -e "s_##VERSION##_${RELEASES[$index]}_g" \
-			-e "s_##DIST##_${DISTS[$index]}_g" "${BASE}/isolinux.cfg_pro" \
-			>> "${BOOT}/isolinux/sipwise_pro.cfg"
-		if [ "${CARRIER[$index]}" == "yes" ]; then
-			sed -e "s_##VERSION##_${RELEASES[$index]}_g" \
-				-e "s_##DIST##_${DISTS[$index]}_g" "${BASE}/isolinux.cfg_carrier" \
-				>> "${BOOT}/isolinux/sipwise_carrier.cfg"
-		fi
-	fi
-done
+echo "*** isolinux templates for RELEASE:${MR} DIST:${DIST} ***"
+
+sed -e "s_##VERSION##_${MR}_g" \
+	-e "s_##DIST##_${DIST}_g" \
+	"${BASE}/isolinux.cfg" > "${BOOT}/isolinux/isolinux.cfg"
