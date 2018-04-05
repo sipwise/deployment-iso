bef502342913aa3d369a378d0f1886e402c15b81
Mykola Malkov <mmalkov@sipwise.com>
2018-04-05 16:16:45 +0200

    TT#32751 Fix stdout issue with dialog utility
    Change-Id: Ic3e814c190f696c7e822b9eaf3311437582951a7

--- templates/scripts/main.sh
+++ templates/scripts/main.sh
@@ -103,10 +103,10 @@ prompt_for_target()
                 echo "${i}" "${disk_info}"
               done) )
 
-  if ! TARGET_DISK=$(dialog --title "Disk selection" --single-quoted --stdout \
+  if ! TARGET_DISK=$({ dialog --title "Disk selection" --single-quoted \
                        --ok-label OK --cancel-label Exit \
                        --menu "Please select the target disk for installing Debian/ngcp:" 0 0 0 \
-                       "${DISK_LIST[@]}") ; then
+                       "${DISK_LIST[@]}" 2>&1; } > /dev/tty) ; then
     ewarn "Cancelling as requested by user during disk selection." ; eend 0
     return 1
   fi
