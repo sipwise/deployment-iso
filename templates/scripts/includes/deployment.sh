f88168e6ccc0fa900f641ca0b69dbb5e76b35a06
Mykola Malkov <mmalkov@sipwise.com>
2018-06-12 10:51:55 +0200

    TT#37257 Use systemctl for reboot and shutdown
    Use systemctl (reboot|poweroff) instead of sysrq triggers, we do not need
    this workaround in a new grml.
    
    Collect the list of running process before and after kills in the end
    of deployment to catch if we do not stop them properly.
    
    Remove outdated checks of jessie release.
    
    Some shellcheck improvements.
    
    Remove PROFILE variable as it does not exist/used.
    
    Change-Id: Idda4ddf490d76e74da74d83afb919a6fa759d0e0

--- templates/scripts/includes/deployment.sh
+++ templates/scripts/includes/deployment.sh
@@ -904,7 +904,6 @@ if "$PRO_EDITION" ; then
   echo "
   Host Role:         $ROLE
   Host Role Carrier: $CROLE
-  Profile:           $PROFILE
 
   External NW iface: $EXTERNAL_DEV
   Ext host IP:       $EXTERNAL_IP
@@ -923,8 +922,8 @@ fi
 if "$INTERACTIVE" ; then
   echo "WARNING: Execution will override any existing data!"
   echo "Settings OK? y/N"
-  read a
-  if [[ "$a" != "y" ]] ; then
+  read -r a
+  if [[ "${a,,}" != "y" ]] ; then
     echo "Exiting as requested."
     exit 2
   fi
@@ -1031,9 +1030,9 @@ check_for_supported_disk() {
   echo "WARNING: Cannot detect supported device vendor/model." >&2
   echo "(Disk: ${DISK}  Vendor: ${disk_vendor}  Model: ${disk_model})" >&2
   echo "Would you like to continue anyway? (yes/NO)" >&2
-  read a
-  case "$a" in
-    y|Y|yes|YES)
+  read -r a
+  case "${a,,}" in
+    y|yes)
       echo "Continue anyway as requested."
       return 0
       ;;
@@ -1274,6 +1273,9 @@ echo FSCK=no >>/etc/debootstrap/config
 
 # package selection
 cat > /etc/debootstrap/packages << EOF
+# update: we need only the following packages
+# eject locales-all firmware-bnx2 firmware-bnx2x ethtool acpi
+# all others are already present in debian stretch
 # addons: packages which d-i installs but debootstrap doesn't
 eject
 grub-pc
@@ -1315,28 +1317,13 @@ ca-certificates
 #kbd
 #laptop-detect
 #os-prober
-EOF
-
-echo  "Adding ifenslave package (because we're installing ${DEBIAN_RELEASE})"
-logit "Adding ifenslave package (because we're installing ${DEBIAN_RELEASE})"
-cat >> /etc/debootstrap/packages << EOF
 # support bonding
 ifenslave
-EOF
-
-echo  "Adding linux-headers-amd64 package (because we're installing ${DEBIAN_RELEASE})"
-logit "Adding linux-headers-amd64 package (because we're installing ${DEBIAN_RELEASE})"
-cat >> /etc/debootstrap/packages << EOF
 # required for dkms
 linux-headers-amd64
-EOF
-
-if "$LVM" ; then
-  cat >> /etc/debootstrap/packages << EOF
 # support LVM
 lvm2
 EOF
-fi
 
 if [ -n "$FIRMWARE_PACKAGES" ] ; then
   cat >> /etc/debootstrap/packages << EOF
@@ -1383,14 +1370,10 @@ logit "Setting up /etc/debootstrap/etc/apt/sources.list"
 cat > /etc/debootstrap/etc/apt/sources.list << EOF
 # Set up via deployment.sh for grml-debootstrap usage
 deb ${MIRROR} ${DEBIAN_RELEASE} main contrib non-free
+deb ${SEC_MIRROR} ${DEBIAN_RELEASE}-security main contrib non-free
+deb ${MIRROR} ${DEBIAN_RELEASE}-updates main contrib non-free
+deb ${DBG_MIRROR} ${DEBIAN_RELEASE}-debug main contrib non-free
 EOF
-
-echo "deb ${SEC_MIRROR} ${DEBIAN_RELEASE}-security main contrib non-free" >> /etc/debootstrap/etc/apt/sources.list
-echo "deb ${MIRROR} ${DEBIAN_RELEASE}-updates main contrib non-free" >> /etc/debootstrap/etc/apt/sources.list
-
-if [ "$DEBIAN_RELEASE" != "jessie" ] ; then
-  echo "deb ${DBG_MIRROR} ${DEBIAN_RELEASE}-debug main contrib non-free" >> /etc/debootstrap/etc/apt/sources.list
-fi
 
 if [ "$DEBIAN_RELEASE" = "stretch" ] && [ ! -r /usr/share/debootstrap/scripts/stretch ] ; then
   echo  "Enabling stretch support for debootstrap via symlink to sid"
@@ -1591,11 +1574,8 @@ EOF
 deb ${MIRROR} ${DEBIAN_RELEASE} main contrib non-free
 deb ${SEC_MIRROR} ${DEBIAN_RELEASE}-security main contrib non-free
 deb ${MIRROR} ${DEBIAN_RELEASE}-updates main contrib non-free
+deb ${DBG_MIRROR} ${DEBIAN_RELEASE}-debug main contrib non-free
 EOF
-
-  if [ "$DEBIAN_RELEASE" != "jessie" ] ; then
-    echo "deb ${DBG_MIRROR} ${DEBIAN_RELEASE}-debug main contrib non-free" >> "$TARGET/etc/apt/sources.list.d/debian.list"
-  fi
 
   # support testing rc releases without providing an according installer package ahead
   if [ -n "$AUTOBUILD_RELEASE" ] ; then
@@ -2187,7 +2167,7 @@ check_puppet_rerun() {
   if ! checkBootParam nopuppetrepeat && [ "$(get_deploy_status)" = "error" ] ; then
     echo "Do you want to [r]epeat puppet run or [c]ontinue?"
     while true; do
-      read a
+      read -r a
       case "${a,,}" in
         r)
           echo "Repeating puppet run."
@@ -2223,7 +2203,7 @@ check_puppetserver_time() {
     else
       echo "WARNING: time difference between the current server and $PUPPET_SERVER is ${seconds} seconds (bigger than 10 seconds)."
       echo "Please synchronize time and press any key to recheck or [c]ontinue with puppet run."
-      read a
+      read -r a
       case "${a,,}" in
         c)
           echo "Continue ignoring time offset check."
@@ -2422,12 +2402,16 @@ EOF
 
 fi # if [ -n "$PUPPET" ] ; then
 
+# check the processes which are not stopped in installer and running
+ps auxwww || true
 # make sure we don't leave any running processes
 for i in asterisk atd collectd collectdmon dbus-daemon exim4 \
          glusterd glusterfs glusterfsd glusterfs-server haveged monit nscd \
 	 redis-server snmpd voisniff-ng ; do
   killall -9 $i >/dev/null 2>&1 || true
 done
+# check the processes which are not stopped with kills above
+ps auxwww || true
 
 # remove retrieved and generated files
 rm -f ${TARGET}/config_*yml
@@ -2455,7 +2439,6 @@ if "$LVM" ; then
   dmsetup remove_all || true
 fi
 
-# make sure /etc/fstab is up2date
 if ! blockdev --rereadpt "/dev/${DISK}" ; then
   echo "Something on disk /dev/${DISK} (mountpoint $TARGET) seems to be still active, debugging output follows:"
   ps auxwww || true
@@ -2489,40 +2472,25 @@ fi
 # do not prompt when running in automated mode
 if "$REBOOT" ; then
   echo "Rebooting system as requested via ngcpreboot"
-  for key in s u b ; do
-    echo $key > /proc/sysrq-trigger
-    sleep 2
-  done
+  systemctl reboot
 fi
 
 if "$HALT" ; then
   echo "Halting system as requested via ngcphalt"
-
-  for key in s u o ; do
-    echo $key > /proc/sysrq-trigger
-    sleep 2
-  done
+  systemctl poweroff
 fi
 
 echo "Do you want to [r]eboot or [h]alt the system now? (Press any other key to cancel.)"
 unset a
-read a
-case "$a" in
+read -r a
+case "${a,,}" in
   r)
     echo "Rebooting system as requested."
-    # reboot is for losers
-    for key in s u b ; do
-      echo $key > /proc/sysrq-trigger
-      sleep 2
-    done
+    systemctl reboot
   ;;
   h)
     echo "Halting system as requested."
-    # halt(8) is for losers
-    for key in s u o ; do
-      echo $key > /proc/sysrq-trigger
-      sleep 2
-    done
+    systemctl poweroff
   ;;
   *)
     echo "Not halting system as requested. Please do not forget to reboot."
