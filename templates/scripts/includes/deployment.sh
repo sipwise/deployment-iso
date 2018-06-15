78fd9eecc2e896f21029dc86dcd48e0e788b6ce5
Alexander Lutay <alutay@sipwise.com>
2018-06-15 14:55:10 +0200

    TT#38454 Remove 'openssh-server' package from ngcp debootstrap
    NGCP metapackages depends on 'ssh' already. So no need to
    install it during the debootstrap process. So far
    deployment.sh is being used to recovery from Puppet
    (ssh module cares about 'openssh-server/-client already)
    and also for Debian plain installation where we still need
    to install 'openssh-server' manually.
    
    Change-Id: If9c052f0517b1610f0e65ed4a574539b23e23928

--- templates/scripts/includes/deployment.sh
+++ templates/scripts/includes/deployment.sh
@@ -1206,6 +1206,14 @@ lvm_setup
 # otherwise e2fsck fails with "need terminal for interactive repairs"
 echo FSCK=no >>/etc/debootstrap/config
 
+if ! "$NGCP_INSTALLER" ; then
+  echo "Install openssh-server be able to login on the Debian plain system"
+  cat > /etc/debootstrap/packages << EOF
+# be able to login on the system, even if just installing plain Debian
+openssh-server
+EOF
+fi
+
 # WARNING: consider to add NGCP packages to NGCP metapackage!
 cat > /etc/debootstrap/packages << EOF
 # addons: packages which d-i installs but debootstrap doesn't
@@ -1222,9 +1230,6 @@ locales-all
 # too many physical server systems, so just install it by default
 firmware-bnx2
 firmware-bnx2x
-
-# be able to login on the system, even if just installing plain Debian
-openssh-server
 
 # TT#5444 ca-certificates is necessary to wget ngcp-installer over https
 ca-certificates
