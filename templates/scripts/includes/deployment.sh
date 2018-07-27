sipwise/deployment-iso

commit d5df0c0f84ebb04219dadf1862b0a629bac16b99

TT#41500 Run dpkg-reconfigure tzdata to workaround a bug in grml-debootstrap
Currently grml-debootstrap only sets /etc/localtime, but not
/etc/timezone, so this confuses the system with libraries/tools
that read the former and others the latter. Reconfigure tzdata so
that it can get both files into a consistent state.

Change-Id: Id34c1a088809fbac19957a750c31fb8f3ceda893

templates/scripts/includes/deployment.sh
@@ -1288,6 +1288,9 @@ save
 EOT
 fi
 
+# TT#41500: Make sure the timezone setup is coherent
+grml-chroot "$TARGET" dpkg-reconfigure tzdata
+
 # provide useable swap partition
 echo "Enabling swap partition $SWAP_PARTITION via /etc/fstab"
 cat >> "${TARGET}/etc/fstab" << EOF

commit ca0a54ba761815c37cdfbdda8fa1c4eecf48dd81

TT#41500 Run dpkg-reconfigure in noninteractive mode
We should not be prompting anything during installation.

Change-Id: I58147d8ae8712c75bff5fc71f213aebe7b4a10d9
Fixes: commit d5df0c0f84ebb04219dadf1862b0a629bac16b99

templates/scripts/includes/deployment.sh
@@ -1289,7 +1289,7 @@ EOT
 fi
 
 # TT#41500: Make sure the timezone setup is coherent
-grml-chroot "$TARGET" dpkg-reconfigure tzdata
+DEBIAN_FRONTEND=noninteractive grml-chroot "$TARGET" dpkg-reconfigure tzdata
 
 # provide useable swap partition
 echo "Enabling swap partition $SWAP_PARTITION via /etc/fstab"
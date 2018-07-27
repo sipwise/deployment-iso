ca0a54ba761815c37cdfbdda8fa1c4eecf48dd81
Guillem Jover <gjover@sipwise.com>
2018-07-27 15:10:13 +0200

    TT#41500 Run dpkg-reconfigure in noninteractive mode
    We should not be prompting anything during installation.
    
    Change-Id: I58147d8ae8712c75bff5fc71f213aebe7b4a10d9
    Fixes: commit d5df0c0f84ebb04219dadf1862b0a629bac16b99

--- templates/scripts/includes/deployment.sh
+++ templates/scripts/includes/deployment.sh
@@ -1289,7 +1289,7 @@ EOT
 fi
 
 # TT#41500: Make sure the timezone setup is coherent
-grml-chroot "$TARGET" dpkg-reconfigure tzdata
+DEBIAN_FRONTEND=noninteractive grml-chroot "$TARGET" dpkg-reconfigure tzdata
 
 # provide useable swap partition
 echo "Enabling swap partition $SWAP_PARTITION via /etc/fstab"
