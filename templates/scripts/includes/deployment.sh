18892b02886bd71e3cf86b5ef241aa91006d6f47
Sergii Kipot <skipot@sipwise.com>
2018-09-19 18:39:22 +0200

    TT#35952 Freeze puppet version to v5.5
    Mcollective is deprecated in v6.0 and I don't see
    any quick fix for that. So let's keep using v5.5
    while deciding how to proceed further.
    
    Change-Id: Ie72e5a57afc0a7235f9d7d29d52af7d796db1ea8

--- templates/scripts/includes/deployment.sh
+++ templates/scripts/includes/deployment.sh
@@ -1951,7 +1951,7 @@ puppet_install_from_puppet () {
 
   echo "Installing 'puppet-agent' with dependencies"
   cat >> ${TARGET}/etc/apt/sources.list.d/puppetlabs.list << EOF
-deb ${DEBIAN_URL}/puppetlabs/ ${DEBIAN_RELEASE} main puppet dependencies
+deb ${DEBIAN_URL}/puppetlabs/ ${DEBIAN_RELEASE} main puppet5 dependencies
 EOF
 
   PUPPET_GPG_KEY="6F6B15509CF8E59E6E469F327F438280EF8D349F"
