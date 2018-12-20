5dc444bdc956186e53441fd1f3095d5bba85aa73
Manuel Montecelo <mmontecelo@sipwise.com>
2018-12-20 11:57:51 +0100

    TT#49058 Change way to detect home of user "sipwise"
    In newer systems it can be under ngcp-data partition (/ngcp-data/home/sipwise),
    in older systems without this partition it can be under /var/sipwise.
    
    Also this way is more future-proof, if the location changes again.
    
    Change-Id: If2d3a3b55ea81871071bf846c8ca981e703d3d88

--- templates/scripts/includes/deployment.sh
+++ templates/scripts/includes/deployment.sh
@@ -1771,7 +1771,11 @@ vagrant_configuration() {
   fi
 
   if "$NGCP_INSTALLER" ; then
-    SIPWISE_HOME="/var/sipwise"
+    local SIPWISE_HOME="/does/not/exist"
+    SIPWISE_HOME=$(chroot "${TARGET}" getent passwd 'sipwise' | cut -d':' -f6)
+    if [[ ! -d "${TARGET}/${SIPWISE_HOME}" ]] ; then
+      die "Error: cannot determine home of 'sipwise' user, it does not exist or not a directory: ${TARGET}/${SIPWISE_HOME}"
+    fi
 
     # TODO: move PATH adjustment to ngcp-installer (ngcpcfg should have a template here)
     if ! grep -q '^# Added for Vagrant' "${TARGET}/${SIPWISE_HOME}/.profile" 2>/dev/null ; then
