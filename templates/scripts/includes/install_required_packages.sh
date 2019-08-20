6fc40e993d2423e47406bd90c26ccd060b497f97
Michael Prokop <mprokop@sipwise.com>
2019-08-20 18:17:00 +0200

    TT#65055 Do not rely on apt sources.list setup of live system
    The Debian security repository layout was modified.  Nowadays only
    `http://security.debian.org/dists/testing-security/` exists and
    `http://security.debian.org/ testing/updates' is gone.
    
    Quoting from https://lists.debian.org/debian-security/2019/06/msg00015.html:
    
    | I would like to switch to *-security instead of */updates starting with
    | bullseye.  There will likely be some complications, but they should be
    | solvable by the time we will publish packages in bullseye-security.
    
    Since we're invoking `apt-get update` on the Grml live system, without
    checking what's configured inside apt's sources.list setup, installation
    might[1] fail with:
    
    | E: The repository 'http://security.debian.org testing/updates Release' does not have a Release file.
    
    Instead set up a custom temporary apt configuration, similar to what
    we're already doing in ensure_packages_installed() of deployment.sh.
    
    [1] This depends on the version of the underlying Grml ISO.  ISO builds
    being based on grml-live commit 3e832a3a10 or newer include a modified
    sources.list setup that's supposed to be working fine.
    
    Change-Id: I420c189115cc173847a826832f7dbb76324cb418

--- templates/scripts/includes/install_required_packages.sh
+++ templates/scripts/includes/install_required_packages.sh
@@ -2,8 +2,35 @@
 
 set -e
 
+# detect the underlying Debian version of the Grml ISO,
+# keeping code backwards compatible for e.g. cherry-picking
+if grep -q buster /etc/debian_version ; then
+  DEBIAN_RELEASE="buster"
+else
+  DEBIAN_RELEASE="stretch"
+fi
+
+# Do not rely on apt sources.list setup of the running live system
+TMPDIR=$(mktemp -d)
+mkdir -p "${TMPDIR}/etc/preferences.d" "${TMPDIR}/statedir/lists/partial" \
+  "${TMPDIR}/cachedir/archives/partial"
+chown _apt -R "${TMPDIR}"
+
+echo "deb https://debian.sipwise.com/debian/ ${DEBIAN_RELEASE} main contrib non-free" > \
+  "${TMPDIR}/etc/sources.list"
+
 echo "Updating list of packages..."
-apt-get update 1>/dev/null
+DEBIAN_FRONTEND='noninteractive' apt-get \
+  -o dir::cache="${TMPDIR}/cachedir" \
+  -o dir::state="${TMPDIR}/statedir" \
+  -o dir::etc="${TMPDIR}/etc" \
+  -o dir::etc::trustedparts="/etc/apt/trusted.gpg.d/" \
+  update >/dev/null
+
 echo "Installing required packages..."
-apt-get install --assume-yes \
-  isomd5sum
+DEBIAN_FRONTEND='noninteractive' apt-get \
+  -o dir::cache="${TMPDIR}/cachedir" \
+  -o dir::state="${TMPDIR}/statedir" \
+  -o dir::etc="${TMPDIR}/etc" \
+  -o dir::etc::trustedparts="/etc/apt/trusted.gpg.d/" \
+  --assume-yes install isomd5sum
