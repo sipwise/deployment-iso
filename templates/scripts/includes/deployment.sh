sipwise/deployment-iso

commit 6661b04af09e87d0620cd316788ecc590b288684

TT#15305 deployment.sh: support trunk-weekly
Change-Id: Ie98ac5fa0de848cf54a96039af5532eb8012bab9

templates/scripts/includes/deployment.sh
@@ -805,8 +805,17 @@ get_installer_path() {
 
   # use a separate repos for trunk releases
   if $TRUNK_VERSION ; then
-    local repos_base_path="${SIPWISE_URL}/autobuild/dists/release-trunk-${DEBIAN_RELEASE}/main/binary-amd64/"
-    INSTALLER_PATH="${SIPWISE_URL}/autobuild/pool/main/n/ngcp-installer/"
+    case "${SP_VERSION}" in
+      trunk)
+        local repos_base_path="${SIPWISE_URL}/autobuild/dists/release-trunk-${DEBIAN_RELEASE}/main/binary-amd64/"
+        INSTALLER_PATH="${SIPWISE_URL}/autobuild/pool/main/n/ngcp-installer/"
+        ;;
+      trunk-weekly)
+        local repos_base_path="${SIPWISE_URL}/autobuild/release/release-${SP_VERSION}/main/binary-amd64/"
+        INSTALLER_PATH="${SIPWISE_URL}/autobuild/release/release-${SP_VERSION}/pool/main/n/ngcp-installer/"
+        ;;
+      *) die "Error: unknown TRUNK_VERSION ${SP_VERSION}" ;;
+    esac
   fi
 
   if [ -n "${NGCP_PPA}" ] ; then
@@ -1566,7 +1575,7 @@ if pgrep dhclient &>/dev/null ; then
   DHCP=true
 fi
 
-if [[ "${SP_VERSION}" == "trunk" ]]; then
+if [[ ${SP_VERSION} =~ ^trunk ]]; then
   TRUNK_VERSION=true
 fi
 

commit b372471a20615f1c358e9fc4b0940378a00ff051

TT#15305 Fix ngcp-deployment-scripts usage for daily-build-matrix-debian-boxes
Git commit 6661b04af09e87d0 broke all our bullseye based builds
(debian, sipwise + docker), see
https://jenkins.mgm.sipwise.com/view/All/job/daily-build-matrix-debian-boxes/

For plain Debian installations we don't have SP_VERSION available,
so default to what was used before supporting trunk-weekly next
to trunk.

Change-Id: I61958f0c67d165d2f6dcb059fe4991ed24a328c9

templates/scripts/includes/deployment.sh
@@ -761,7 +761,12 @@ retrieve_deployment_scripts_fake_uname() {
       local repos_base_path="${SIPWISE_URL}/autobuild/release/release-${SP_VERSION}/dists/release-${SP_VERSION}/main/binary-amd64/"
       local deployment_path="${SIPWISE_URL}/autobuild/release/release-${SP_VERSION}/pool/main/n/ngcp-deployment-iso/"
       ;;
-    *) die "Error: unknown TRUNK_VERSION ${SP_VERSION}" ;;
+    *)
+      echo "NOTE: SP_VERSION is unset, assuming we're installing a non-NGCP system"
+      echo "NOTE: using fake-uname.so of ngcp-deployment-scripts from release-trunk-${DEBIAN_RELEASE}"
+      local repos_base_path="${SIPWISE_URL}/autobuild/dists/release-trunk-${DEBIAN_RELEASE}/main/binary-amd64/"
+      local deployment_path="${SIPWISE_URL}/autobuild/pool/main/n/ngcp-deployment-iso"
+      ;;
   esac
 
   wget --timeout=30 -O Packages.gz "${repos_base_path}Packages.gz"